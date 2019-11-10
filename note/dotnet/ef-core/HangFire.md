f5af5367e039ef3aa15b12f4dffbf2a37b3e3b8e
Alice52 <zzhang_xz@163.com>
2019-11-10 19:51:25 +0800

    fix: refine directory

---
+++ note/dotnet/ef-core/HangFire.md
@@ -0,0 +1,39 @@
+## HangFire Job
+
+### Introduce
+
+- Requirements¶
+  ```c#
+  .NET Framework 4.5
+  Persistent storage (listed below)
+  Newtonsoft.Json library ≥ 5.0.1
+  ```
+
+### Quick Start
+
+### Storage Engine
+
+- SQLServer
+- MSMQ
+- Redis
+- Mongo
+
+### Job Type
+
+- backgroung job
+- reccuring job
+- fire-and-forget job
+- delayed job
+- continuative job
+
+### Retry
+
+### configuration
+
+- queue-name
+- rating
+- job-id
+- timezone
+- job process
+
+### Distributed
---
+++ project/csharp/Syntax/Syntax/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var result = "{\"张三\":[1, 2]}";
+            Dictionary<string, IList<int>> dic2 = JsonConvert.DeserializeObject<Dictionary<string, IList<int>>>(result);
+
+
+            var result2 = "{ \"zack\": 1 ,\"张三\":[1, 2]}";
+            Dictionary<Dictionary<string, int>, Dictionary<string, IList<int>>> dic2 = JsonConvert.DeserializeObject<Dictionary<string, int>, Dictionary<string, IList<int>>>(result2);
+            Console.WriteLine(result);
+
+            string s = "[{\"ConfiguredChipSets\": [{\"Key\": 1, \"Value\": []},{ \"Key\": 2,\"Value\": [\"HKD-NN-5\"]}]}]";
+            string s2 = "{\"ConfiguredChipSets\": [{1,  []},{  2, [\"HKD-NN-5\"]}]}";
+
+            IDictionary<string, IList<string>> ConfiguredChipSets;
+
+            ConfiguredChipSets = JsonConvert.DeserializeObject<IDictionary<string, IList<string>>>(s);
+
+            Console.WriteLine(ConfiguredChipSets);
+
+
+
+        }
+
+            
+    }
+}
